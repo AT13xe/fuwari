9ad0aa9265de2a755d368a0dacb573f1b137058a
AT13xe <845043294@qq.com>
2025-08-09 19:17:13 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -41,9 +41,9 @@ export const siteConfig: SiteConfig = {
 	favicon: [
 		// Leave this array empty to use the default favicon
 		{
-		     src: '',    // Path of the favicon, relative to the /public directory
+		     src: 'assets/images/5.jpg',    // Path of the favicon, relative to the /public directory
 		//   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+		     sizes: '720x720',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
 		}
 	],
 };
